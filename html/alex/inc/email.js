d4752306db8db8faac3bb6732b6641b7f8500276
Alex Lauderdale <tzolkat@gmail.com>
2019-05-14 15:01:28 -0400

    Changed anti-spam script to run on DOM ready instead of body onload.

--- html/alex/inc/email.js
+++ html/alex/inc/email.js
@@ -1,14 +1,16 @@
-function renderEmail() {
-	var o = "tzolkat";
-	var n = "net";
-	var m = "alex";
-	var l = document.getElementsByClassName("emailLink");
-	var t = document.getElementsByClassName("emailText");
-	var i = 0;
-	for (i = 0; i < l.length; i++) {
-		l[i].setAttribute("href", "mailto:" + m + "@" + o + "." + n);
-	}
-	for (i = 0; i < t.length; i++) {
-		t[i].innerHTML = (m + "@" + o + "." + n);
+document.onreadystatechange = function() {
+	if (document.readyState == "interactive") {
+		var o = "tzolkat";
+		var n = "net";
+		var m = "alex";
+		var l = document.getElementsByClassName("emailLink");
+		var t = document.getElementsByClassName("emailText");
+		var i = 0;
+		for (i = 0; i < l.length; i++) {
+			l[i].setAttribute("href", "mailto:" + m + "@" + o + "." + n);
+		}
+		for (i = 0; i < t.length; i++) {
+			t[i].innerHTML = (m + "@" + o + "." + n);
+		}
 	}
 }

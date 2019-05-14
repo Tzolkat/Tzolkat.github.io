8a38cb2052d8daf6589b7224467287a07aafc7d3
Alex Lauderdale <tzolkat@gmail.com>
2019-05-14 17:11:26 -0400

    Let Javascript insert some boilerplate HTML surrounding articles.

---
+++ html/alex/inc/util.js
@@ -0,0 +1,45 @@
+// Personal Site Page Utilities (c) 2019 Alex Lauderdale.
+// --------------------------------------------------------------------------------------
+// ID selectors: top.
+// Class selectors: emailLink, emailText, totop.
+// --------------------------------------------------------------------------------------
+// Note: IE8 is not supported.
+// ======================================================================================
+
+// Converts a NodeList into an array so we can iterate over it and such.
+function toArray(nodeList) {
+	return Array.prototype.slice.call(nodeList);
+}
+
+// Anti-Spam email function. Fills in href and content for email tags.
+function renderEmail() {
+	var o = "tzolkat";
+	var n = "net";
+	var m = "alex";
+	toArray(document.querySelectorAll(".emailLink")).forEach(function(item) {
+		item.setAttribute("href", "mailto:" + m + "@" + o + "." + n);
+	});
+	toArray(document.querySelectorAll(".emailText")).forEach(function(item) {
+		item.innerHTML = (m + "@" + o + "." + n);
+	});
+}
+
+// Adds back to top functionality and dividers to each article except the first.
+function articleFeatures() {
+	document.querySelector("header").setAttribute("id", "top");
+	toArray(document.querySelectorAll("article")).slice(1).forEach(function(item) {
+		var el = document.createElement("a");
+		el.setAttribute("class", "totop");
+		el.setAttribute("href", "#top");
+		el.innerHTML = "&uarr; Back to Top";
+		item.appendChild(el);
+		el = document.createElement("hr");
+		item.parentNode.insertBefore(el, item);
+	});
+}
+
+// This is the stuff we do once the DOM is loaded.
+document.addEventListener("DOMContentLoaded", function() {
+	renderEmail();
+	articleFeatures();
+});

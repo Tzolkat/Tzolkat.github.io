dce666bc9ddd1bf5ad9092c750ec47b3983a513f
Alex Lauderdale <tzolkat@gmail.com>
2019-05-22 00:18:14 -0400

    Test prism plugins.

---
+++ html/alex/assets/downloads/hello_world.java
@@ -0,0 +1,9 @@
+// Syntax highlighting: https://prismjs.com/
+// This is a Java 'Hello World' program.
+int main(String[] args)
+{
+	System.out.println("This is a simple Hello World program!");
+
+	int sum = 2 + 3;
+	System.out.println("The sum is: " + sum);
+}

360fdeaf799be0695ed36bfe08a1d35e1727f7a8
Alex Lauderdale <tzolkat@gmail.com>
2019-05-22 03:22:35 -0400

    Added named variable referencing.

--- html/alex/assets/downloads/hello_world.java
+++ html/alex/assets/downloads/hello_world.java
@@ -4,6 +4,6 @@ int main(String[] args)
 {
 	System.out.println("This is a simple Hello World program!");
 
-	int sum = 2 + 3;
-	System.out.println("The sum is: " + sum);
+	int myTotal = 2 + 3;
+	System.out.println("The sum is: " + myTotal);
 }
